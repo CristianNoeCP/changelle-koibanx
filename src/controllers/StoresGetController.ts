c7eb60a6b145a8867d1eb7613c96c7b84c269ecd
CristianNoeCP <cristiannoecardenasparra@gmail.com>
2022-08-16 20:24:31 -0500

    quryparams get

--- src/controllers/StoresGetController.ts
+++ src/controllers/StoresGetController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
 import { Controller } from "./Controller";
-
 export class StoresGetController implements Controller {
   constructor() {}
 
   async run(_req: Request, res: Response) {
-    res.status(httpStatus.OK).send();
+    const { query: queryParams } = _req;
+    const { filters, limit, offset } = queryParams;
+
+    res.status(httpStatus.OK).send(filters);
   }
 }
